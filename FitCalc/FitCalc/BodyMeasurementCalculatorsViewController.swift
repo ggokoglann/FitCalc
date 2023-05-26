4c071c7c0b43d5a148338eeff829d10d7d5b6224
Gökhan Gökoğlan <119362605+ggokoglann@users.noreply.github.com>
2023-05-26 16:07:29 +0300

    Add files via upload

---
+++ FitCalc/FitCalc/BodyMeasurementCalculatorsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BodyMeasurementCalculatorsViewController.swift
+//  FitCalc
+//
+//  Created by Gökhan Gökoğlan on 26.05.2023.
+//
+
+import UIKit
+
+class BodyMeasurementCalculatorsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
