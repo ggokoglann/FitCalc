0c6c92567cf7840e03da201357e6c9e6219cf0d5
Gökhan Gökoğlan <119362605+ggokoglann@users.noreply.github.com>
2023-05-26 21:39:04 +0300

    Add files via upload

---
+++ FitCalc/FitCalc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FitCalc
+//
+//  Created by Gökhan Gökoğlan on 3.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
