c93e732844d9af0f7ca01528b59553747f36bcbc
Gökhan Gökoğlan <119362605+ggokoglann@users.noreply.github.com>
2023-05-30 12:03:51 +0300

    Add files via upload

---
+++ FitCalc/FitCalc/BodyMeasurementCalculatorsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  BodyMeasurementCalculatorsViewController.swift
+//  FitCalc
+//
+//  Created by Gökhan Gökoğlan on 26.05.2023.
+//
+
+import UIKit
+
+class BodyMeasurementCalculatorsViewController: UIViewController {
+    @IBOutlet var idealWeightCalculator: UIButton!
+    @IBOutlet var bmiCalculator: UIButton!
+    @IBOutlet var bodyFatCalculator: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FitCalc/FitCalc/CalculatorViewController.swift
@@ -0,0 +1,31 @@
+//
+//  CalculatorViewController.swift
+//  FitCalc
+//
+//  Created by Gökhan Gökoğlan on 3.05.2023.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+    @IBOutlet var calorieCalculator: UIButton!
+    @IBOutlet var bmrCalculator: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
