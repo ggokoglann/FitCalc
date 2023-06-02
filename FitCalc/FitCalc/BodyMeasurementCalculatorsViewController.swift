bb2b24b640a3efb628eb4ceaf859cb341f72808c
Gökhan Gökoğlan <119362605+ggokoglann@users.noreply.github.com>
2023-06-02 17:49:19 +0300

    Add files via upload

---
+++ FitCalc/FitCalc/BodyMeasurementCalculatorsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  BodyMeasurementCalculatorsViewController.swift
+//  FitCalc
+//
+//  Created by Gökhan Gökoğlan on 26.05.2023.
+//
+
+import UIKit
+
+class BodyMeasurementCalculatorsViewController: UIViewController {
+    @IBOutlet var idealWeightCalculator: UIButton!
+    @IBOutlet var bmiCalculator: UIButton!
+    @IBOutlet var bodyFatCalculator: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+}
